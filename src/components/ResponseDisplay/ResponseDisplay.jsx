b793f0044afc91bca05680b811debda8e3ec76b8
DanielGamma <daniel.carrasco@gammatech.school>
2023-03-15 13:27:02 +0100

    Change display bg color

--- src/components/ResponseDisplay/ResponseDisplay.jsx
+++ src/components/ResponseDisplay/ResponseDisplay.jsx
@@ -2,7 +2,7 @@ import { JsonViewer } from "@textea/json-viewer";
 
 export default function ResponseDisplay({ response }) {
     return (
-        <section className="fixed top-0 right-0 w-display-width h-screen bg-slate-200 overflow-y-scroll">
+        <section className="fixed top-0 right-0 w-display-width h-screen bg-slate-100 overflow-y-scroll">
             {
                 response ?
                     <JsonViewer value={response} /> :
