db7add2f9dad71380cb78ab3e343fbfc6637266a
DanielGamma <daniel.carrasco@gammatech.school>
2023-03-15 16:24:25 +0100

    Add nonreferrer to links

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ export default function Hero() {
     <header className="flex flex-col justify-start items-start gap-2 w-main-width ml-navbar-width p-8">
         <h1 className="text-5xl font-bold">Café de Altura Docs</h1>
         <h2 className="text-2xl font-medium">Welcome to the documentation of the Café de Altura API.</h2>
-        <p>Café de Altura is a Frontend project that simulates a professional coffee e-commerce. It is designed and developed by <a href="https://www.gammatech.school/" target={"_blank"} className="text-blue-700 font-medium underline hover:text-red-500">GammaTech School</a> for its students of the <span className="font-medium">Frontend Developer Bootcamp</span>. As part of the project, it provides a <span className="font-medium">REST API</span> to manage the site's products content. Here you will find the documentation of said API, all the endpoints, and a testing framework to get used to its data structures.</p>
+        <p>Café de Altura is a Frontend project that simulates a professional coffee e-commerce. It is designed and developed by <a href="https://www.gammatech.school/" target={"_blank"} rel="noreferrer" className="text-blue-700 font-medium underline hover:text-red-500">GammaTech School</a> for its students of the <span className="font-medium">Frontend Developer Bootcamp</span>. As part of the project, it provides a <span className="font-medium">REST API</span> to manage the site's products content. Here you will find the documentation of said API, all the endpoints, and a testing framework to get used to its data structures.</p>
     </header>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ export default function NavBar() {
 
   return (
     <nav className="fixed top-0 left-0 flex flex-col gap-8 w-navbar-width min-h-screen p-4 bg-gray-900 text-slate-200">
-      <a href="https://www.gammatech.school/" target={"_blank"}><img src={logo} alt="" /></a>
+      <a href="https://www.gammatech.school/" target={"_blank"} rel="noreferrer"><img src={logo} alt="" /></a>
       <h3 className="text-2xl font-medium">Endpoints:</h3>
       <ul className="flex flex-col justify-start items-start gap-3">
         <a href="#get-all-products" className="cursor-pointer text-xl hover:text-white">Get All Products</a>
